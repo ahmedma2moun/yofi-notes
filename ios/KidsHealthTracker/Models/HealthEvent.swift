7082fe8fcefe336bb08c5c1498be112f4428ee27
Ahmed Maamoun <ahmedfcisasu@gmail.com>
2026-05-23 18:46:21 +0300

    fixing issue

--- ios/KidsHealthTracker/Models/HealthEvent.swift
+++ ios/KidsHealthTracker/Models/HealthEvent.swift
@@ -19,12 +19,10 @@ struct AnyCodable: Codable {
 
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        if let intValue = try? container.decode(Int.self) {
-            value = intValue
+        if let boolValue = try? container.decode(Bool.self) {
+            value = boolValue
         } else if let doubleValue = try? container.decode(Double.self) {
             value = doubleValue
-        } else if let boolValue = try? container.decode(Bool.self) {
-            value = boolValue
         } else if let stringValue = try? container.decode(String.self) {
             value = stringValue
         } else {
